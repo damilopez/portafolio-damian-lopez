ee3ee6e5e0dbff30941cba93ecd410e8bb8626fe
damilopez <damiangabriellopez@gmail.com>
2023-04-08 18:01:18 -0300

    Agregada responsividad para breakpoints 720px y 420px

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,20 @@ desplazarArriba.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// Selecciones para Barra de navegación hamburguesa
+const burger = document.querySelector("#menu-hamburguesa");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
